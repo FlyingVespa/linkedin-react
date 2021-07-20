8b6252d6dc3f00075e1ea4324d611e17db6b57b1
Helena Nel <mailhedri@gmail.com>
2021-07-20 16:12:08 +0200

    Network List of users added

--- src/components/Network/Network.jsx
+++ src/components/Network/Network.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import { Container, Row, Col, Card, Button, Image } from "react-bootstrap";
 import { useState, useEffect } from "react";
-// import { Avatar } from "@material-ui/core";
-// import banner from "./banner.jpg";
+import { Avatar } from "@material-ui/core";
+import banner from "./banner.jpg";
 // import "../HomePage/SidebarLeft/SidebarLeft.css";
 import "./network.css";
 import { XLg } from "react-bootstrap-icons";
 import { Nav } from "react-bootstrap";
 import dotenv from "dotenv";
 dotenv.config();
-
 const NetworkFeed = () => {
-  const token = process.env.REACT_APP_TOKEN;
+  const token =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGM1MzExZDcwNDBkZjAwMTU4NWM4MDIiLCJpYXQiOjE2MjY3NzE4OTQsImV4cCI6MTYyNzk4MTQ5NH0.qQBwLrP9YhLV6i04gO7-VYpUyY0fHe9U1J9cfptWNi4";
 
   const [profiles, setProfiles] = useState([]);
   const getProfile = async () => {
-    console.log("FIRST BEER");
     try {
       let response = await fetch(
         "https://striveschool-api.herokuapp.com/api/profile/",
         {
           method: "GET",
           headers: {
-            Authorization: "Bearer" + token,
+            Authorization: token,
             "Content-Type": "application/json",
           },
         }
@@ -52,10 +51,10 @@ const NetworkFeed = () => {
             return (
               <Col style={{ padding: "10px" }}>
                 <Card className="networkFeedCard">
-                  <Image className="feedLeft-cover-img" />
+                  <Image className="feedLeft-cover-img" src={banner} />
                   <XLg className="xCricleFill" />
 
-                  {/* <Avatar
+                  <Avatar
                     variant="top"
                     src={profile.image}
                     style={{
@@ -66,7 +65,7 @@ const NetworkFeed = () => {
                       marginLeft: "60px",
                       marginTop: "30px",
                     }}
-                  /> */}
+                  />
                   <Card.Body>
                     <Card.Title
                       className="networkFeed"
